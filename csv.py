cb6767a680f5756d618667100148a9bd600f8c4e
Umi101108 <408465808@qq.com>
2017-05-24 17:46:23 +0800

    csv操作

---
+++ csv.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import csv
+
+with open('filename', 'w') as csvfile:
+	writer = csv.writer(csvfile, dialect = 'excel')
+	writer.writerow([('col1', 'col2', 'col3')])
+	for item in items:
+		writer.writerow([tuple(item)])
