f185e4ca7813c28ba21994d7788793e964a7e478
Umi101108 <408465808@qq.com>
2017-05-25 16:21:13 +0800

    remove

--- csv.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import csv
-
-with open('filename', 'w') as csvfile:
-	writer = csv.writer(csvfile, dialect = 'excel')
-	writer.writerow([('col1', 'col2', 'col3')])
-	for item in items:
-		writer.writerow([tuple(item)])
