d3731ee22495afad5d89f05c35d750ca7bb61d3e
Umi101108 <408465808@qq.com>
2017-06-01 17:03:44 +0800

    csv
    编码问题

--- Basis/csv.py
+++ Basis/csv.py
@@ -6,4 +6,4 @@ with open('filename', 'w') as csvfile:
 	writer = csv.writer(csvfile, dialect = 'excel')
 	writer.writerow([('col1', 'col2', 'col3')])
 	for item in items:
-		writer.writerow([tuple(item)])
+		writer.writerow([tuple(item).encode('utf8')])
