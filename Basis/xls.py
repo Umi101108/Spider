cd0343099e76087cdba89a087daba8246325b49a
Umi101108 <408465808@qq.com>
2017-06-05 14:22:51 +0800

    read
    加入读模块

--- Basis/xls.py
+++ Basis/xls.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import xlwt
+import xlwt  # 写模块
+import xlrd  # 读模块
 
+# read 
+wb = xlrd.open_workbook('table.xls')
+sheet = wb.sheets()[0]
+for i in range(5):
+	if i == 0:
+		continue
+	str = sheet.row_values(i)[2]
+	print str
+
+# write
 newTable = 'table.xls'
 wb = xlwt.Workbook(encoding='utf-8')
 ws = wb.add_sheet('sheet1')
-ws.write(0, 2, ['col1'], xlwt.easyxf('font: bold on'))  # 行 列 数据
+for i in range(10):
+	ws.write(i, 2, ['col1'], xlwt.easyxf('font: bold on'))  # 行 列 数据
 wb.save(newTable)
