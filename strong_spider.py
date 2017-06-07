2815942da3b8ef36248c41854bddb88b963e12b1
Umi101108 <408465808@qq.com>
2017-06-07 17:43:18 +0800

    增加工具类
    剔除标签换行符等元素

--- strong_spider.py
+++ strong_spider.py
@@ -45,3 +45,36 @@ class Spider(object):
             return response.text
         else:
             return None
+
+
+
+class Tool:
+
+    # 将超链接广告剔除
+    removeADLink = re.compile('<div clas"link_layer.*?</div>')
+    # 去除img标签，1-7位空格，&nbsp;
+    removeImg = re.compile('<img.*?>| {1,7}|&nbsp;')
+    # 删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    # 把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    # 将表格制表<td>替换为\t
+    replaceTD = re.compile('<td>')
+    # 将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    # 将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+    # 将多行空格删除
+    removeNoneLine = re.compile('\n+')
+
+    def replace(self, x):
+        x = re.sub(self.removeADLink, "", x)
+        x = re.sub(self.removeImg, "", x)
+        x = re.sub(self.removeAddr, "", x)
+        x = re.sub(self.replaceLine, "\n", x)
+        x = re.sub(self.replaceTD, "\t", x)
+        x = re.sub(self.replaceBR, "\n", x)
+        x = re.sub(self.removeExtraTag, "", x)
+        x = re.sub(self.removeNoneLine, "\n", x)
+        # strip()将前后多余内容删除
+        return x.strip()
