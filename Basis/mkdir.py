76c5d1dbda33fb9951621cdcba1692ea4ab88134
Umi101108 <408465808@qq.com>
2017-06-27 17:19:46 +0800

    dir

---
+++ Basis/mkdir.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import os
+
+pwd = 'D:\\pictures\\' or '/User/umi/pictures'
+
+def mkdir(self, path):
+	path = path.strip()
+	isExists = os.path.exists(os.path.join(pwd, path))
+	if not isExists:
+		print "新建了一个名为" + path + "的文件夹"
+		os.makedirs(os.path.join(pwd, path))
+		return True
+	else:
+		print "名字叫做" + path + "的文件夹已经存在了"
+		return True
