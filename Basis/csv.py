45884106f62f318bca2039e4b9b2ad072ec1b2da
Umi101108 <408465808@qq.com>
2017-07-11 17:44:29 +0800

    csv 文件读写

--- Basis/csv.py
+++ Basis/csv.py
@@ -2,8 +2,18 @@
 
 import csv
 
+
+with open('filename', 'rb') as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		print row
+
 with open('filename', 'w') as csvfile:
 	writer = csv.writer(csvfile, dialect = 'excel')
 	writer.writerow([('col1', 'col2', 'col3')])
+	items = [
+			('xiaoming', 'china', '10'),
+			('lina', 'USA', '12')
+		]
 	for item in items:
 		writer.writerow([tuple(item).encode('utf8')])
