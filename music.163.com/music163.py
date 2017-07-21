e262c7a7aba4f5e1aa2c082afecfed70ff156e79
wangwei <408465808@qq.com>
2017-07-21 23:32:25 +0800

    success

--- music.163.com/music163.py
+++ music.163.com/music163.py
@@ -4,13 +4,15 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import selenium.webdriver.support.ui as ui
 import time
 
 class Music(object):
 
 	def __init__(self):
-		self.base_url = "http://music.163.com/m/user/home?id=371821058"
-		self.songs_url = "http://music.163.com/#/user/songs/rank?id=371821058"
+		self.base_url = "http://music.163.com/user/home?id=371821058"
+		self.rank_url = "http://music.163.com/user/songs/rank?id=371821058"
+		self.playlist_url = "http://music.163.com/playlist?id=525591083"
 		self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
 		self.headers = {'User-Agent': self.user_agent}
 
@@ -24,7 +26,7 @@ class Music(object):
 			print "该网页不存在"
 			return
 
-	def getInfo(self):
+	def getUserInfo(self):
 		soup = self.getSoup(self.base_url)
 		name = soup.select('h2#j-name-wrap > span')[0].get_text()
 		level = soup.select('h2#j-name-wrap > span')[1].get_text()
@@ -37,57 +39,79 @@ class Music(object):
 		event_count = soup.select('ul#tab-box > li > a > strong')[0].get_text()
 		follow_count = soup.select('ul#tab-box > li > a > strong')[1].get_text()
 		fan_count = soup.select('ul#tab-box > li > a > strong')[2].get_text()
-		weibo = soup.select('a[class="u-slg u-slg-sn]"')[0].get('href')
+		try:
+			weibo = soup.select('a[class="u-slg u-slg-sn"]')[0].get('href')
+		except:
+			weibo = None
 		print name, level, gender, weibo
 
+	def getFavPlaylist(self):
+		# driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
+		driver = webdriver.PhantomJS("/Users/umi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+		driver.get(self.base_url)
+		driver.switch_to_frame('g_iframe')
+		try:
+			wait = ui.WebDriverWait(driver, 15)
+			wait.until(lambda driver: driver.find_element_by_xpath('//*[@class="m-cvrlst f-cb"]/li[1]/div/a'))
+			urls = driver.find_elements_by_xpath('//*[@class="m-cvrlst f-cb"]/li[1]/div/a')
+			# print urls.text
+			favourite_url = urls[0].get_attribute("href")
+			print favourite_url
+			print urls[1].get_attribute("href")
+			# print driver.page_source
+		except Exception as e:
+			print e
+		# finally:
+		# 	driver.quit()
 
-	def getSongs(self):
-		soup = self.getSoup(self.songs_url)
-		# print soup.select('div.g-bd')[0].get_text()
-		driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
+	def getFavSongs(self):
+		driver = webdriver.PhantomJS("/Users/umi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+		driver.get(self.playlist_url)
+		driver.switch_to_frame('g_iframe')
+		m_table = driver.find_elements_by_xpath('//*[@class="j-flag"]/table/tbody/tr')
+		for m in m_table:
+			# print '|'.join(m.text.split('\n'))
+			no = m.find_element_by_xpath('./td[@class="left"]').text
+			song_id = m.find_element_by_xpath('.//div[@class="f-cb"]//a::href')
+			song_title = m.find_element_by_xpath('.//div[@class="f-cb"]//b').text
+			artist = m.find_element_by_xpath('.//div[@class="text"]/span').text
+			album_name = m.find_element_by_xpath('.//div[@class="text"]/a[last()]').text
+			print song_id, no, song_title, artist, album_name
+
+	def getRank2(self):
+		# driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
+		driver = webdriver.PhantomJS("/Users/umi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
 		driver.get(self.songs_url)
 		print driver.page_source
 
-
-	def main(self):
-		# url='http://music.163.com/weapi/v1/resource/comments/R_SO_4_465920636?csrf_token=c0f6bfdcd0526ec0ba6c207051a08960'
-		url = 'http://music.163.com/#/user/songs/rank?id=371821058'
+	def getRank(self):
 		url = 'http://music.163.com/weapi/v1/play/record?csrf_token='
-		refer = 'http://music.163.com/user/songs/rank?id=371821058'
-		# param={'params':'dEaDmN4/oYqVIhuN9BgCa3KEqFuhYu8zNhFLWrAS5R9sWzxoP3uBypNQkthPXVvUPxhDlAfftKcjkVhrEpAOAR7jrphSw6rtHpBtQi658UFRJMr3dZDT5nSIqwtoEpSDQyYTzead16sG44sEsIWHpiXTqRwG/xzWuiMJ6pFV1cRTWKSk5z/o9vanYfuVHeFx','encSecKey':'bdd80b128c4ab823d380f6013d6503f9af156fc47bd48b8bfe970c0b66dd1f9921f6da19d803025a7bb06647144d76f5b803bd88a39a79d576a041a690dc9e70dcba41bd651b7f002a7babf9ed4ab138edbf2022cbaa5f52d05dac0a472b2bcbf6ab930649f6e6edb17a3ae200001dab31a6c8e2203e87576c2b596d51571c03'}#这里每行末尾的‘\’是代码过长用来换行的，慎用，换行多了易出现bug。
+		refer = 'http://music.163.com/user/songs/rank?id=371821054'
 		headers = {
 			'Referer': refer,
 			'User-Agent': self.user_agent,
-			'Cookie': 'JSESSIONID-WYYY=IhihdCdm%2F01pIoDVftVVoi4a%5CwI8x9OtmHMoj5lonFu7UTenllimfBgWOnCTiozJi%2BiEifpmYUCJNxqEAneGEneunZP6wh1lNmv6OfhAKP9uG8NsEs9Cdrmdqao7KMSsQIY%2FWlDnx5UjZIkhdE4FgV%2FzAmkJoJ5YMgfIUEXBduzAzuvE%3A1500475598484; _iuqxldmzr_=32; _ntes_nnid=67a88fb8cc9127d324f706770305c34d,1500473798525; _ntes_nuid=67a88fb8cc9127d324f706770305c34d; __utma=94650624.743015140.1500473799.1500473799.1500473799.1; __utmb=94650624.8.10.1500473799; __utmc=94650624; __utmz=94650624.1500473799.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
 		}
 		params = {
 			'params': 'mIp+51R7s1mx9dW0rcoymal+gFM0z+Ici0kPqMNFIklgBuX3RkCLndhvLcZi43hwZMCHF9QvjrrS/ojUUbW9b84IHCzmI/E/HY4TvH5IWkmdTsHMqv3wwvaqfZpEe/f5ZXlOUdWkFDNdaA8gJqK1/juqxp6k6AV1ySY0+2XLmWt0tXkbtzD09CCZKXFLEiU6',
 			'encSecKey': 'a4bf46891389db284457d131a4dbd5b6de90c39265a9552c69f3a666c87a84a3c386e9b5ebc81c20b6d8c34b2f39996ffb95d077354354e2a796eb884ae36ddc6a7260c961d1954201d398c7df7c1a23273deb436bbd6c0721fcfee61e507b82f275b162b6c192a4738f19b4ee46860c3007d64e705f92f72e25adb4daf5392d',
 		}
-		# r=requests.get(url, headers=headers)
 		r = requests.post(url, headers=headers, data=params)
 		data = json.loads(r.content)
-		# for d in data:
-		# 	print d
-		# print data['weekData']
 		all_songs = data.get('allData')
 		for songs in all_songs:
-			print songs.get('song').get('name')
-			print songs.get('song').get('ar')[0].get('name')
+			song_score = songs.get('score')
+			song_id = songs.get('song').get('id')
+			song_name = songs.get('song').get('name')
+			artist_id = songs.get('song').get('ar')[0].get('id')
+			artist_name = songs.get('song').get('ar')[0].get('name')
+			album_id = songs.get('song').get('al').get('id')
+			album_name = songs.get('song').get('al').get('name')
+			print song_id, song_name, artist_id, artist_name, album_id, album_name, song_score
 
-		# data=r.text#data得到的就是json
-		# print data
-		# self.getInfo()
-		# driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
-		# driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
-		# driver.get(self.base_url)
-		# time.sleep(5)
-		# elem_songsall = driver.find_element_by_xpath('//*[@id="songsall"]')
-		# elem_songsall.click()
-		# time.sleep(5)
-		# elem_songsmore = driver.find_element_by_xpath('//div[@class="more"]/a')
-		# elem_songsmore.click()
-		# self.getSongs()
+	def main(self):
+		# self.getFavPlaylist()
+		self.getFavSongs()
+		# self.getRank()
 
 
 if __name__ == "__main__":
--- music.163.com/sele.py
+++ music.163.com/sele.py
@@ -5,7 +5,7 @@ import selenium.webdriver.support.ui as ui
 import time
 
 # driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
-# driver = webdriver.PhantomJS("/Users/umi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+driver = webdriver.PhantomJS("/Users/umi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
 # driver.get('http://music.163.com/m/user/home?id=371821058')
 # driver.switch_to_frame('g_iframe')
 # elem_songsall = driver.find_element_by_xpath('//*[@id="songsall"]')
@@ -13,14 +13,21 @@ import time
 # elem_more = driver.find_element_by_xpath('//div[@class="more"]')
 # elem_more.click()
 # print driver.page_source
-songs_url = "http://music.163.com/#/user/songs/rank?id=371821058"
-driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
+songs_url = "http://music.163.com/user/songs/rank?id=371821058"
+# driver = webdriver.Chrome("/Users/umi/Downloads/chromedriver")
 driver.get(songs_url)
 driver.switch_to_frame('g_iframe')
-wait = ui.WebDriverWait(driver, 15)
-wait.until(lambda driver: driver.find_element_by_xpath('//*[@id="songsall"]'))
+# wait = ui.WebDriverWait(driver, 15)
+# wait.until(lambda driver: driver.find_element_by_xpath('//*[@id="songsall"]'))
 # elem_songsall = driver.find_element_by_xpath('//*[@id="songsall"]')
-elem_songsall.click()
+# elem_songsall.click()
 
-songs = driver.find_elements_by_xpath('//div[@id="m-record"]')
-print songs[0].text
+songs_record = driver.find_element_by_xpath('//div[@id="m-record"]')
+# print driver.page_source
+# print song
+time.sleep(2)
+songs = songs_record.find_elements_by_xpath('//li')
+print driver.page_source
+for song in songs:
+    print song.text
+
