57434e85ca7623b9af44eedd577f854275cbad5e
wangwei <408465808@qq.com>
2017-08-10 07:05:11 +0800

    zhihu

---
+++ www.zhihu.com/ZhihuSpider/ZhihuSpider/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'umi'
+__date__ = '2017/8/10 上午7:03'
+
+from scrapy.cmdline import execute
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "zhihu"])
--- www.zhihu.com/ZhihuSpider/ZhihuSpider/spiders/zhihu.py
+++ www.zhihu.com/ZhihuSpider/ZhihuSpider/spiders/zhihu.py
@@ -3,10 +3,12 @@
 import re
 import json
 import time
+import datetime
 from urlparse import urljoin
 from PIL import Image
 import scrapy
-
+from scrapy.loader import ItemLoader
+from ZhihuSpider.items import ZhihuQuestionItem, ZhihuAnswerItem
 
 class ZhihuSpider(scrapy.Spider):
     name = "zhihu"
@@ -96,4 +98,52 @@ class ZhihuSpider(scrapy.Spider):
                 yield scrapy.Request(url, headers=self.headers, callback=self.parse)
 
 
+    def parse_question(self, response):
+        # 处理question页面，从页面中提取出具体的question item
+        if "QuestionHeader-title" in response.text:
+            # 处理新版本
+            match_obj = re.match("(.*?zhihu.com/question/(\d+))(/|$).*?", response.url)
+            if match_obj:
+                question_id = int(match_obj.group(2))
 
+            item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+            item_loader.add_css("title", "h1.QuestionHeader-title::text")
+            item_loader.add_css("content", ".QuestionHeader-detail")
+            item_loader.add_value("url", response.url)
+            item_loader.add_value("zhihu_id", question_id)
+            item_loader.add_css("answer_num", ".List-headerText span::text")
+            item_loader.add_css("comments_num", ".QuestionHeader-actions button::text")
+            item_loader.add_css("watch_user_num", ".NumberBoard-value::text")
+            item_loader.add_css("topics", ".QuestionHeader-topics .Popover div::text")
+            question_item = item_loader.load_item()
+        # else:
+        #     # 处理老版本页面的item提取
+        #     ma
+        yield scrapy.Request(self.start_answer_url.format(question_id, 20, 0), headers=self.headers, callback=self.parse_answer)
+        yield question_item
+
+
+    def parse_answer(self, response):
+        # 处理question的answer
+        ans_json = json.load(response.text)
+        is_end = ans_json["paging"]["is_end"]
+        next_url = ans_json["paging"]["next"]
+
+        # 提取answer的具体字段
+        for answer in ans_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["praise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
