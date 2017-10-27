1ca4cc681883873c69dae2d10992afc7cf4c8c99
Umi101108 <408465808@qq.com>
2017-10-27 17:44:09 +0800

    add cfda 国产药品

--- www.sfda.gov.cn/gcyp.py
+++ www.sfda.gov.cn/gcyp.py
@@ -2,14 +2,15 @@
 import requests
 import re
 
-url = 'http://qy1.sfda.gov.cn/datasearch/face3/content.jsp?tableId=25&tableName=TABLE25&tableView=%B9%FA%B2%FA%D2%A9%C6%B7&Id=29813'
 
-response = requests.get(url)
-html = response.content
-results = re.findall('<tr>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', html, re.S)
-a = {}
-for r in results:
-	a[r[0]] = r[1]
+for i in xrange(2000, 2010):
+	url = 'http://qy1.sfda.gov.cn/datasearch/face3/content.jsp?tableId=25&tableName=TABLE25&tableView=%B9%FA%B2%FA%D2%A9%C6%B7&Id={}'.format(str(i))
+	response = requests.get(url)
+	html = response.content
+	results = re.findall('<tr>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', html, re.S)
+	a = {}
+	for r in results:
+		a[r[0]] = r[1]
 
-for k, v in a.iteritems():
-	print k, re.sub('<.*?>', '', v)
+	for k, v in a.iteritems():
+		print k, re.sub('<.*?>', '', v)
