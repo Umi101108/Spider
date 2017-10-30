054f8500e004d12496780a52290d4d225a51abff
Umi101108 <408465808@qq.com>
2017-10-30 17:40:23 +0800

    del 多余未知字段

--- kyfw.12306.cn/tickets.py
+++ kyfw.12306.cn/tickets.py
@@ -22,7 +22,6 @@ class TrainsCollection(object):
 		for raw_train in self.available_trains:
 			data = raw_train.split('|')
 			train_no = data[2]
-			# print data[3], data[32]
 			station_train_code = data[3]
 			start_station_telecode = data[4]
 			start_station_name = self.reverse_station[start_station_telecode]
@@ -34,35 +33,9 @@ class TrainsCollection(object):
 			to_station_name = self.reverse_station[to_station_telecode]
 			start_time = data[8]
 			arrive_time = data[9]
-			# day_difference
-			# train_class_name
 			lishi = data[10]
 			canWebBuy = data[11]
-			# lishiValue
-			# yp_info
-			# control_train_day
 			start_train_date = data[13]
-			# seat_feature
-			# yp_ex
-			# train_seat_feature
-			# seat_type
-			# location_code
-			# from_station_no
-			# to_station_no
-			# control_train_day
-			# sale_time
-			# is_support_card
-			# note 
-			# controlled_train_flag
-			# controlled_train_message
-			# gg_num
-			# gr_num
-			# qt_num
-			# rz_num
-			# tz_num
-			# yb_num
-			# ze_num
-			# zy_num
 			rw_num = data[23]
 			wz_num = data[26]
 			yw_num = data[28]
@@ -72,7 +45,6 @@ class TrainsCollection(object):
 			swz_num = data[32]
 			gjrw = data[33]
 			train = [station_train_code, from_station_name+'-'+to_station_name, start_time+'-'+arrive_time, lishi, swz_num, ydz_num, edz_num, rw_num, yw_num, yz_num, wz_num]
-			# '车次 车站 时间 历时 一等 二等 软卧 硬卧 硬座 无座'
 			# print str(train).decode('string_escape')
 			yield train
 		
@@ -88,8 +60,9 @@ def cli():
 	base_url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={date}&leftTicketDTO.from_station={from_station}&leftTicketDTO.to_station={to_station}&purpose_codes=ADULT'
 	from_station = station.get('上海')
 	to_station = station.get('杭州')
-	url = base_url.format(date='2017-10-28', from_station=from_station, to_station=to_station)
+	url = base_url.format(date='2017-11-01', from_station=from_station, to_station=to_station)
 	response = requests.get(url, verify=False)
+	print response.content
 	result = response.json()['data']['result']
 	
 	TrainsCollection(result, '2').pretty_print()
