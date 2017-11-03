195fb901e22fa857640303d797f5ca955418eba5
wangwei <408465808@qq.com>
2017-11-03 19:38:41 +0800

    Revert "add 直接访问这条路看来是封死了"
    This reverts commit 691d4fceb5245a6146c58e4741bd2dfeb5f57d0f.

--- www.sfda.gov.cn/gcyp.py
+++ www.sfda.gov.cn/gcyp.py
@@ -1,31 +1,16 @@
 # coding: utf8
 import requests
 import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-# driver = webdriver.Chrome("D:\chromedriver_win32\chromedriver.exe")
-# driver.get('http://qy1.sfda.gov.cn/datasearch/face3/content.jsp?tableId=25&tableName=TABLE25&tableView=%B9%FA%B2%FA%D2%A9%C6%B7&Id=233')
-# elem = driver.find_element_by_xpath('')
-# keyword = ''.encode('utf8')
-# elem.send_keys(keyword, Keys.RETURN)
-
-# html = driver.page_source
-# print html
 
 
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-headers = {'User-Agent': user_agent}
-for i in xrange(233, 234):
+for i in xrange(2000, 2010):
 	url = 'http://qy1.sfda.gov.cn/datasearch/face3/content.jsp?tableId=25&tableName=TABLE25&tableView=%B9%FA%B2%FA%D2%A9%C6%B7&Id={}'.format(str(i))
-	response = requests.get(url, headers=headers)
+	response = requests.get(url)
 	html = response.content
-	print html
-# 	# results = re.findall('<tr>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', html, re.S)
-# 	# a = {}
-# 	# for r in results:
-# 	# 	a[r[0]] = r[1]
-# 	# 	print r
+	results = re.findall('<tr>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', html, re.S)
+	a = {}
+	for r in results:
+		a[r[0]] = r[1]
 
-# 	# for k, v in a.iteritems():
-# 	# 	print k, re.sub('<.*?>', '', v)
+	for k, v in a.iteritems():
+		print k, re.sub('<.*?>', '', v)
