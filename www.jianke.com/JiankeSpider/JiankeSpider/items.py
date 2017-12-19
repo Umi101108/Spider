57c1c62d494d381c06986e549a760fd37e476083
Umi101108 <408465808@qq.com>
2017-12-19 17:41:40 +0800

    完成说明书结构解析

--- www.jianke.com/JiankeSpider/JiankeSpider/items.py
+++ www.jianke.com/JiankeSpider/JiankeSpider/items.py
@@ -15,20 +15,33 @@ class JiankespiderItem(scrapy.Item):
 
 
 class DrugItem(scrapy.Item):
-    commonname
-    tradename
-    ingredients
-    characters
-    indications
-    specification
-    dosageandadministration
-    adversereactions
-    contraindications
-    cautions
-    list_interaction
-    storage
-    package
-    usefullife
-    implementstandard
-    approvalno
-    corporationname
+    commonname = scrapy.Field()
+    tradename = scrapy.Field()
+    warningsmarks = scrapy.Field()
+    ingredients = scrapy.Field()
+    characters = scrapy.Field()
+    radioactivityandtime = scrapy.Field()
+    actioncategory = scrapy.Field()
+    indications = scrapy.Field()
+    specification = scrapy.Field()
+    dosageandadministration = scrapy.Field()
+    adversereactions = scrapy.Field()
+    contraindications = scrapy.Field()
+    warning = scrapy.Field()
+    cautions = scrapy.Field()
+    pregnancyandnursingmothers = scrapy.Field()
+    pediatricuse = scrapy.Field()
+    geriatricuse = scrapy.Field()
+    list_interaction = scrapy.Field()
+    overdosage = scrapy.Field()
+    clinicaltrails = scrapy.Field()
+    pharmacologicalandtoxicological = scrapy.Field()
+    pharmacokinetics = scrapy.Field()
+    storage = scrapy.Field()
+    package = scrapy.Field()
+    usefullife = scrapy.Field()
+    implementstandard = scrapy.Field()
+    approvalno = scrapy.Field()
+    registerno = scrapy.Field()
+    importlicenceno = scrapy.Field()
+    corporationname = scrapy.Field()
--- www.jianke.com/JiankeSpider/JiankeSpider/spiders/jianke.py
+++ www.jianke.com/JiankeSpider/JiankeSpider/spiders/jianke.py
@@ -2,6 +2,7 @@
 import re
 import scrapy
 from scrapy.http import Request
+from JiankeSpider.items import DrugItem
 
 
 class JiankeSpider(scrapy.Spider):
@@ -24,26 +25,40 @@ class JiankeSpider(scrapy.Spider):
         results = re.findall('<p>.*?<em>(.*?)</em>(.*?)</p>', direction, re.S)
         commonname = re.findall(u'通用名称：(.*?)</p> ', direction, re.S)
         tradename = re.findall(u'商品名称：(.*?)</p> ', direction, re.S)
-        print commonname[0], tradename[0]
         raw = {}
         for r in results:
-            print r[0], r[1]
             raw[r[0]] = r[1]
-        commonname
-        tradename
-        ingredients = raw.get('', '')
-        characters
-        indications
-        specification
-        dosageandadministration
-        adversereactions
-        contraindications
-        cautions
-        list_interaction
-        storage
-        package
-        usefullife
-        implementstandard
-        approvalno
-        corporationname
+        item = DrugItem()
+        item['commonname'] = commonname[0]
+        item['tradename'] = tradename[0]
+        item['warningsmarks'] = raw.get(u'【警示语】', '')
+        item['ingredients'] = raw.get(u'【主要成份】', '')
+        item['characters'] = raw.get(u'【性 状】', '')
+        item['radioactivityandtime'] = raw.get(u'【放射性活度和标识时间】', '')
+        item['actioncategory'] = raw.get(u'【作用类别】', '')
+        item['indications'] = raw.get(u'【适应症/功能主治】', '')
+        item['specification'] = raw.get(u'【规格型号】', '')
+        item['dosageandadministration'] = raw.get(u'【用法用量】', '')
+        item['adversereactions'] = raw.get(u'【不良反应】', '')
+        item['contraindications'] = raw.get(u'【禁 忌】', '')
+        item['warning'] = raw.get(u'【警告】', '')
+        item['cautions'] = raw.get(u'【注意事项】', '')
+        item['pregnancyandnursingmothers'] = raw.get(u'【孕妇及哺乳期妇女用药】', '')
+        item['pediatricuse'] = raw.get(u'【儿童用药】', '')
+        item['geriatricuse'] = raw.get(u'【老年患者用药】', '')
+        item['list_interaction'] = raw.get(u'【药物相互作用】', '')
+        item['overdosage'] = raw.get(u'【药物过量】', '')
+        item['clinicaltrails'] = raw.get(u'【临床试验】', '')
+        item['pharmacologicalandtoxicological'] = raw.get(u'【药理毒理】', '')
+        item['pharmacokinetics'] = raw.get(u'【药代动力学】', '')
+        item['storage'] = raw.get(u'【贮 藏】', '')
+        item['package'] = raw.get(u'【包 装】', '')
+        item['usefullife'] = raw.get(u'【有 效 期】', '')
+        item['implementstandard'] = raw.get(u'【执行标准】', '')
+        item['approvalno'] = raw.get(u'【批准文号】', '')
+        item['registerno'] = raw.get(u'【进口药品注册证号】', '')
+        item['importlicenceno'] = raw.get(u'【进口许可证号】', '')
+        item['corporationname'] = raw.get(u'【生产企业】', '')
+        for k, v in item.iteritems():
+            print k, v
         pass
